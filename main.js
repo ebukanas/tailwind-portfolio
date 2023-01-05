fbbfb4ad8179a12b34cb01b8615e47a253a33814
ebukanas <edvinas.bukanas@gmail.com>
2023-01-05 16:10:03 +0200

    publish

---
+++ main.js
@@ -0,0 +1,121 @@
+var menu = document.getElementById('menu');
+
+function toggleMenu() {
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('w-full');
+    menu.classList.toggle('h-screen');
+}
+
+const header = document.querySelector('[data-header]')
+const sections = [...document.querySelectorAll('[data-section]')]
+const logo = document.querySelector('.logo')
+// const email = document.querySelectorAll('.email-logo')
+// const phone = document.querySelectorAll('.phone-logo')
+// const workHide = document.querySelector('.whitespace')
+// const hamburgerMenu = document.querySelectorAll('.line')
+const menuToggle = document.querySelector('.menuToggle')
+const workShowcase2 = document.getElementById('work2') 
+
+
+
+
+
+let prevYPosition = 0
+let direction = 'up'
+
+const options = {
+	rootMargin: `${header.offsetHeight * -1}px`,
+	threshold: 0
+}
+
+const getTargetSection = (entry) => {
+	const index = sections.findIndex((section) => section == entry.target)
+
+	if (index >= sections.length - 1) {
+	 return entry.target
+	} else {
+	 return sections[index + 1]
+	}
+}
+
+//sets the default header colors to white on load
+// window.addEventListener("load", () => {
+// 	logo.setAttribute('data-logo', "purple")
+// 	mobile_nav_menu_toggle.forEach((obj) => {
+// 		obj.setAttribute('data-mobile_menu_lines', "purple");
+// 	})
+// })
+
+
+const updateColors = (target) => {
+	const theme = target.dataset.section
+	header.setAttribute('data-theme', theme)
+    header.setAttribute('data-header', theme)
+	logo.setAttribute('data-logo', theme)
+	menuToggle.setAttribute('data-menu', theme)
+
+
+
+    // workHide.setAttribute('data-hide', theme)
+
+
+	// mobile_nav_menu_toggle.forEach((obj) => {
+	// 	obj.setAttribute('data-mobile_menu_lines', theme);
+	// })
+
+	// email.forEach((obj) => {
+	// 	obj.setAttribute('data-email', theme);
+	// })
+
+	// phone.forEach((obj) => {
+	// 	obj.setAttribute('data-phone', theme);
+	// })
+
+	// hamburgerMenu.forEach((obj) => {
+	// 	obj.setAttribute('data-nav', theme);
+	// })
+	
+  console.log('background color is ' + theme);
+}
+
+const shouldUpdate = (entry) => {
+	if (direction === 'down' && !entry.isIntersecting) {
+		return true
+	}
+
+	if (direction === 'up' && entry.isIntersecting) {
+		return true
+	}
+
+	return false
+}
+
+
+const onIntersect = (entries) => {
+	entries.forEach((entry) => {
+		if (window.scrollY > prevYPosition) {
+			direction = 'down'
+		} else {
+			direction = 'up'
+		}
+
+		prevYPosition = window.scrollY
+
+		const target = direction === 'down' ? getTargetSection(entry) : entry.target
+
+		if (shouldUpdate(entry)) {
+			updateColors(target)
+		}
+	})
+}
+
+const observer = new IntersectionObserver(onIntersect, options)
+
+sections.forEach((section) => {
+	observer.observe(section)
+})
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./**/*.{html}"],
+  theme: {
+    fontFamily: {
+      'futura': ['futura', 'bold'],
+    },
+    extend: {
+      height: {
+        '550': '550px',
+        '350': '350px',
+      },
+      backgroundImage: {
+        'logo-black': "url('/images/logo.png')",
+        'logo-white': "url('/images/logo_white.png')",
+        'kernagis1': "url('/images/galva2.jpg')",
+        'kernagis2': "url('/images/galva7.jpg')",
+        'kernagis3': "url('/images/galva10.jpg')",
+        'silainiai': "url('/images/silainiai.png')",
+        'ceburekas': "url('/images/chebureki54.jpg')",
+        'hero': "url('/images/statue.png')",
+        'hero2': "url('/images/statue2.png')",
+        'skulptura': "url('/images/skulptura8.jpg')",
+      },
+      backgroundSize: {
+        '550': '550px',
+        '350': '350px',
+        '700': '700px',
+      },
+      backgroundColor: {
+        'purple': '#392c42',
+        'blue': '#0058ff',
+      }
+    },
+  },
+
+  plugins: [],
+}
